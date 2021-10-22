4bcd066c023a1bdf0d64ff480549d3548c4a968f
gowthamgaddamRBEI <91131616+gowthamgaddamRBEI@users.noreply.github.com>
2021-10-22 17:57:24 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,18 @@
 #include "stats.h"
+#include <bits/stdc++.h>
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float>& i_lstData) {
+    Stats tempStatsObj;
+    if(i_lstData.empty())
+    {
+        tempStatsObj.average = NAN;
+        tempStatsObj.min = NAN;
+        tempStatsObj.max = NAN;
+        return tempStatsObj;
+    }
+    tempStatsObj.average = accumulate(i_lstData.begin(), i_lstData.end(), 0)/i_lstData.size();
+    tempStatsObj.min = *min_element(i_lstData.begin(), i_lstData.end());
+    tempStatsObj.max = *max_element(i_lstData.begin(), i_lstData.end());
+    return tempStatsObj;
 }
